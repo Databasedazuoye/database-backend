6cb9716a7a73fb52e28af18e19b3e687d3f7bdad
chenyi <71563698@qq.com>
2023-05-31 20:19:08 +0800

    :bug: fixed utils

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"goodsManagement/router"
-	utils "goodsManagement/util"
+	utils "goodsManagement/utils"
 )
 
 func main() {
 	utils.InitConfig()
 	utils.InitMySQL()
+	utils.InitRedis()
 	r := router.Router()
 	r.Use(utils.Cors())
 	r.Run()
