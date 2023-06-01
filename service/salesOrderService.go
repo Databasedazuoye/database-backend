7d8f6097c151b90458915337556dfc393bbda00b
chenyi <71563698@qq.com>
2023-06-01 23:29:54 +0800

    update cors

--- service/salesOrderService.go
+++ service/salesOrderService.go
@@ -1,15 +1,24 @@
 package service
 
-//func SalesOrderInsert(c *gin.Context) {
-//	warehouseId := utils.StringToInt64(c.Query("warehouseId"))
-//	goodsId := utils.StringToInt64(c.Query("goodsId"))
-//	num := utils.StringToInt64(c.Query("num"))
-//	inventory := dao.InventoryGetByWarehouseIdAndGoodsId(warehouseId, goodsId)
-//	if inventory.Stock < int(num) {
-//		c.JSON(400, gin.H{
-//			"msg": "库存不足，出库失败",
-//		})
-//	}
-//	salesOrder :=
-//
-//}
+import (
+	"github.com/gin-gonic/gin"
+	"goodsManagement/dao"
+	"goodsManagement/utils"
+)
+
+func SalesOrderInsert(c *gin.Context) {
+	warehouseId := utils.StringToInt64(c.Query("warehouseId"))
+	goodsId := utils.StringToInt64(c.Query("goodsId"))
+	num := utils.StringToInt64(c.Query("num"))
+	inventory := dao.InventoryGetByWarehouseIdAndGoodsId(warehouseId, goodsId)
+	if inventory.Stock < int(num) {
+		c.JSON(400, gin.H{
+			"msg": "库存不足，出库失败",
+		})
+	}
+	//salesOrder := model.SalesOrder{
+	//	GoodsId: goodsId,
+	//
+	//}
+
+}
